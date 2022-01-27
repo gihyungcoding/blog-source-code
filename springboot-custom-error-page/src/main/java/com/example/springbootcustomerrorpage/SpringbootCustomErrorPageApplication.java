dfa9be9964c9ca04426a499fe868a1667bb71fd7
gihyungcoding <gihyungcoding@github.com>
2022-01-27 16:11:04 +0900

    ADD : springboot-custom-error-page

---
+++ springboot-custom-error-page/src/main/java/com/example/springbootcustomerrorpage/SpringbootCustomErrorPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcustomerrorpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCustomErrorPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCustomErrorPageApplication.class, args);
+	}
+
+}
---
+++ springboot-custom-error-page/src/test/java/com/example/springbootcustomerrorpage/SpringbootCustomErrorPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcustomerrorpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomErrorPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
