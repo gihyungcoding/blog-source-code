85d3485becb1c7ab89bb0a9d59b086ce9b7fc037
gihyungcoding <gihyungcoding@github.com>
2022-03-31 18:35:13 +0900

    ADD : springboot-async

---
+++ springboot-async/src/main/java/com/example/springbootasync/async/service/AsyncService.java
@@ -0,0 +1,5 @@
+package com.example.springbootasync.async.service;/**
+ * @author gihyung.lee
+ * @since 2022/03/31
+ */public class AsyncService {
+}
---
+++ springboot-async/src/main/java/com/example/springbootasync/config/AsyncConfig.java
@@ -0,0 +1,5 @@
+package com.example.springbootasync.config;/**
+ * @author gihyung.lee
+ * @since 2022/03/31
+ */public class AsyncConfig {
+}
